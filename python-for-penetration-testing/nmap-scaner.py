ec8382e9d5586fb77891fc5fb8952203dee46bcd
Adarsh Choutha <adarshchoutha@gmail.com>
2025-07-15 10:28:03 +0530

    'Developing an Nmap Scanner part 2' - complete [5/7]

--- python-for-penetration-testing/nmap-scaner.py
+++ python-for-penetration-testing/nmap-scaner.py
@@ -16,3 +16,27 @@ Please enter the type of scan you want to run
     3) Comprehensive Scan
 """)
 print(f'You have selected option: {resp}')
+
+if resp == '1':
+    print(f'Nmap Version: {scanner.nmap_version()}')
+    scanner.scan(ip_address, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print('IP Status: ', scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print('Open Ports: ', scanner[ip_address]['tcp'].keys())
+elif resp == '2':
+    print(f'Nmap Version: {scanner.nmap_version()}')
+    scanner.scan(ip_address, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print('IP Status: ', scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print('Open Ports: ', scanner[ip_address]['udp'].keys())
+elif resp == '3':
+    print(f'Nmap Version: {scanner.nmap_version()}')
+    scanner.scan(ip_address, '1-1024', '-v -sS -sV -sC -A -O')
+    print(scanner.scaninfo())
+    print('IP Status: ', scanner[ip_address].state())
+    print(scanner[ip_address].all_protocols())
+    print('Open Ports: ', scanner[ip_address]['tcp'].keys())
+else:
+    print('Please enter a valid option')
